1b8bdbbc28dd6ed83c1f8e871dbd2b1c865a41e7
Rafal Woloszyn <rafal@debugduckdesign.com>
2024-03-11 22:19:52 +0100

    feat: parsing

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,12 @@
 
-fn reverse(pair: (i32, bool)) -> (bool, i32) {
-    let (int_param, bool_param) = pair;
-    (bool_param, int_param)
-}
-
-
-#[derive(Debug)]
-struct Matrix(f32, f32, f32, f32); //TODO: ? 
 
 fn main() {
-    // A tuple with a bunch of different types.
-    let long_tuple = (1u8, 2u16, 3u32, 4u64,
-        -1i8, -2i16, -3i32, -4i64,
-        0.1f32, 0.2f64,
-        'a', true);
+    let guess1:i32 = "42".parse().expect("Not a number!");
+    println!("{:?}", guess1);
 
-    // Values can be extracted from the tuple using tuple indexing.
-    println!("Long tuple first value: {}", long_tuple.0);
-    println!("Long tuple second value: {}", long_tuple.1);
+    let guess2:f32 = "42.22".parse().expect("Not a number!");
+    println!("{:?}", guess2);
 
-    // Tuples can be tuple members.
-    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
-
-    // Tuples are printable.
-    println!("tuple of tuples: {:?}", tuple_of_tuples);
-
-    // But long Tuples (more than 12 elements) cannot be printed.
-    //let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
-    //println!("Too long tuple: {:?}", too_long_tuple);
-    // TODO ^ Uncomment the above 2 lines to see the compiler error
-
-    let pair = (1, true);
-    println!("Pair is {:?}", pair);
-
-    println!("The reversed pair is {:?}", reverse(pair));
-
-    // To create one element tuples, the comma is required to tell them apart
-    // from a literal surrounded by parentheses.
-    println!("One element tuple: {:?}", (5u32,));
-    println!("Just an integer: {:?}", (5u32));
-
-    // Tuples can be destructured to create bindings.
-    let tuple = (1, "hello", 4.5, true);
-
-    let (a, b, c, d) = tuple;
-    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
-
-    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-    println!("{:?}", matrix);
-    }
+    let guess3:u32 = "Test".parse().expect("Not a number!");
+    println!("{:?}", guess3);
+}
