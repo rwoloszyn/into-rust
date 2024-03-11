f7500ba8cd4cc0d6e74f5c740202775019a02db5
Rafal Woloszyn <rafal@debugduckdesign.com>
2024-03-11 22:25:50 +0100

    feat: loop

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 
 
 fn main() {
-    let number = 1232;
+    let mut counter = 0;
 
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
-    } else {
-        println!("number is not divisible by 4, 3, or 2");
-    }
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
 }
