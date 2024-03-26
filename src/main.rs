478ed34d2532b036d60db48414d2f553447b8c85
Rafal Woloszyn <rafal@debugduckdesign.com>
2024-03-26 21:44:01 +0100

    mutability

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
+use std::io;
+fn main() -> std::io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
 
+    let weigth_on_mars = check_mars_weight(100.0);
+    println!("Weight on Mars: {}kg", weigth_on_mars + 1.0);
 
-fn main() {
-    let mut counter = 0;
+    Ok(())
+}
 
-    let result = loop {
-        counter += 1;
-
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-
-    println!("The result is {result}");
+fn check_mars_weight(weight: f32) -> f32 {
+    weight / 9.81 * 3.71
 }
