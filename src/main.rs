61a6bbd16509b8ef1daca6648b4f3ba25e65330d
Rafal Woloszyn <rafal@debugduckdesign.com>
2024-03-26 21:53:38 +0100

    start building http1.1 version

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-use std::io;
-fn main() -> std::io::Result<()> {
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
-
-    let weigth_on_mars = check_mars_weight(100.0);
-    println!("Weight on Mars: {}kg", weigth_on_mars + 1.0);
-
-    Ok(())
+fn main(){
+    let server = HttpServer::new("127.0.0.0:8080");
+    server.run()
 }
 
-fn check_mars_weight(weight: f32) -> f32 {
-    weight / 9.81 * 3.71
+struct HttpServer {
+    addr: String,
+
+}
+
+impl HttpServer {
+    fn new(addr: String) -> Self {
+        HttpServer {
+            addr
+        }
+    }
+
+    fn run(self) {
+        println!("Server is running on {}", self.addr);
+    }
 }
